f3f7a6eec99067edaffe68d26b9aac528e227ae9
gheger <garry@swisclima.ch>
2019-07-17 19:25:39 +0200

    - Commit DB (3 tables imported) - Set up SQLite for EF - Create EDMX - Transform templates

--- BetForMe/BetForMe/MainWindow.xaml.cs
+++ BetForMe/BetForMe/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BetForMe.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,18 @@ namespace BetForMe
         public MainWindow()
         {
             InitializeComponent();
+
+            using (BetForMeDBContainer c = new BetForMeDBContainer()) {
+                var result = from r in c.Germany_18_19 select r;
+                var resultAsList = result.ToList<Germany_18_19>();
+
+                var result2 = from r2 in c.England_18_19 select r2;
+                var resultAsList2 = result2.ToList<England_18_19>();
+
+                var result3 = from r3 in c.Spain_18_19 select r3;
+                var resultAsList3 = result3.ToList<Spain_18_19>();
+            }
+
         }
     }
 }
